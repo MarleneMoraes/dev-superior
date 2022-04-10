d27b1dfe32d464fee43a88213c14a1154e416d2f
Marlene Moraes <marlenevmoraes@outlook.com>
2022-04-09 22:10:17 -0300

    section 3

--- curso-java-completo/src/com/devsuperior/javacompleto/aula018/Main.java
+++ curso-java-completo/src/com/devsuperior/javacompleto/aula018/Main.java
@@ -1,5 +1,12 @@
 package com.devsuperior.javacompleto.aula018;
 
+/**
+ * 
+ * @author Marlene
+ * SEÇÃO 3: Introdução à linguagem Java
+ * AULA 18. Primeiro programa em Java no Eclipse
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
