3c4c2e4bf6a899261f4aaa647a90ea6e142b5ed0
Marlene Moraes <marlenevmoraes@outlook.com>
2022-08-21 12:58:56 -0300

    REST tested

---
+++ curso-spring-boot/src/main/java/com/devsuperior/cursospringboot/resources/CategoryResource.java
@@ -0,0 +1,16 @@
+package com.devsuperior.cursospringboot.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categories")
+public class CategoryResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST is working!";
+	}
+	
+}
