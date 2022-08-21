63774efbf2d15bdd6f5a4a7b14732715f675a313
Marlene Moraes <marlenevmoraes@outlook.com>
2022-08-21 12:40:42 -0300

    curso spring boot

---
+++ curso-spring-boot/src/test/java/com/devsuperior/cursospringboot/CursoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.cursospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
