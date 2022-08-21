37279b6737dc00efcc480bede6d33aa62bd9578f
Marlene Moraes <marlenevmoraes@outlook.com>
2022-08-21 13:16:43 -0300

    domain class category

--- curso-spring-boot/src/main/java/com/devsuperior/cursospringboot/resources/CategoryResource.java
+++ curso-spring-boot/src/main/java/com/devsuperior/cursospringboot/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.devsuperior.cursospringboot.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.cursospringboot.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST is working!";
+	public List<Category> toList() {
+		
+		Category category1 = new Category(1,"Informática");
+		Category category2 = new Category(2,"Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(category1);
+		list.add(category2);
+		
+		return list;
 	}
 	
 }
