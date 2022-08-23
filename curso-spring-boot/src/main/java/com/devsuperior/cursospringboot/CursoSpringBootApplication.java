b01edba48831a6249b15adde13e982845420535e
Marlene Moraes <marlenevmoraes@outlook.com>
2022-08-23 18:47:31 -0300

    create intance of implementation

--- curso-spring-boot/src/main/java/com/devsuperior/cursospringboot/CursoSpringBootApplication.java
+++ curso-spring-boot/src/main/java/com/devsuperior/cursospringboot/CursoSpringBootApplication.java
@@ -1,13 +1,32 @@
 package com.devsuperior.cursospringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpringBootApplication {
+import com.devsuperior.cursospringboot.domain.Category;
+import com.devsuperior.cursospringboot.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursoSpringBootApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringBootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category category1 = new Category(null, "Informática");
+		Category category2 = new Category(null, "Escritório");
+		
+		repository.saveAll(Arrays.asList(category1, category2));
+		
 	}
 
 }
