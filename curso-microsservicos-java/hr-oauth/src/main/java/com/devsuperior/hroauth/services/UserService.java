fd5b2d7e5da54e884cadd7a8cde9f232db64ab64
Marlene Moraes <marlenevmoraes@outlook.com>
2025-11-27 20:02:24 -0300

    User Feign Client

---
+++ curso-microsservicos-java/hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.hroauth.services;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		
+		if (user == null) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		
+		logger.info("Email found: " + email);
+		return user; 
+	}
+}
