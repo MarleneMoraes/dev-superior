66cee8187ec20ac1d921ca0dad5c9bb60950907c
Marlene Moraes <marlenevmoraes@outlook.com>
2025-11-28 19:51:11 -0300

    Zuul Authorization

--- curso-microsservicos-java/hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ curso-microsservicos-java/hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -47,12 +47,6 @@ public class WorkerResource {
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		
-		try {
-			Thread.sleep(3000L);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		return ResponseEntity.ok(repository.findById(id).get());
